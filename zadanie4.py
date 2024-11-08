e02835dd177d3b55ae47f60f63831e09cf7defd8
nedznyziutek <amelia.czajkowska@thinkingzone.pl>
2024-11-08 11:54:04 +0100

    Update zadanie4.py

---
+++ zadanie4.py
@@ -0,0 +1,8 @@
+# Get user input
+user_input = input("Please enter a string to reverse: ")
+
+# Reverse the string
+reversed_string = user_input[::-1]
+
+# Print the reversed string
+print("Reversed string:", reversed_string)
