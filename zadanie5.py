bd0378f50c850e4b97710d1d25d6d1a66a673d36
nedznyziutek <amelia.czajkowska@thinkingzone.pl>
2024-11-08 12:01:38 +0100

    Update zadanie5.py

---
+++ zadanie5.py
@@ -0,0 +1,9 @@
+# Inicjalizacja zmiennej sumy
+suma = 0
+
+# Iteracja po liczbach od 1 do 100
+for i in range(1, 101, 2):
+    suma += i
+
+# Wyświetlenie wyniku
+print("Suma liczb nieparzystych od 1 do 100 wynosi:", suma)
